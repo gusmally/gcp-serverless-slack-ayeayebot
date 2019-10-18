67424b6a3472b613c2b1b66a73e4c395c6043e0a
gusmally <hannahmbmoraes@gmail.com>
2019-10-18 16:58:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -19,34 +19,34 @@ def search_images(term):
         if str(term.lower()) in a:
             imageMatches.append(a)
     if len(imageMatches) > 1:
-		image_url = random.choice(imageMatches)
+	image_url = random.choice(imageMatches)
 	
-		message = {
-			'response_type': 'in_channel',
-			'attachments': []
-		}  	
+	message = {
+		'response_type': 'in_channel',
+		'attachments': []
+	}  	
 		
-		attachment = {}
-		attachment['image_url'] = image_url
-		message['attachments'].append(attachment)
+	attachment = {}
+	attachment['image_url'] = image_url
+	message['attachments'].append(attachment)
         return message
     elif len(imageMatches) == 1:
-		image_url = imageMatches[0]
+	image_url = imageMatches[0]
 		
-		message = {
-			'response_type': 'in_channel',
-			'attachments': []
-		}  
+	message = {
+		'response_type': 'in_channel',
+		'attachments': []
+	}  
 		
-		attachment = {}
-		attachment['image_url'] = image_url
-		message['attachments'].append(attachment)
+	attachment = {}
+	attachment['image_url'] = image_url
+	message['attachments'].append(attachment)
         return message
     else:
-		message = {
-			'response_type': 'ephemeral',
-			'text': 'no aye ayes matched that term :( _this form will hopefully one day support submissions_',
-		}  
+	message = {
+		'response_type': 'ephemeral',
+		'text': 'no aye ayes matched that term :( _this form will hopefully one day support submissions_',
+	}  
         return message
 
 def acronym_bot(request):
